379c2ba624b87acee4b965076afc0dd3d3dd5dd7
Brian Osborn <bosborn@gmail.com>
2021-07-19 13:57:33 -0600

    simple and compound crs

---
+++ crs-ios/CRSCompoundCoordinateReferenceSystem.m
@@ -0,0 +1,108 @@
+//
+//  CRSCompoundCoordinateReferenceSystem.m
+//  crs-ios
+//
+//  Created by Brian Osborn on 7/19/21.
+//  Copyright © 2021 NGA. All rights reserved.
+//
+
+#import "CRSCompoundCoordinateReferenceSystem.h"
+
+@implementation CRSCompoundCoordinateReferenceSystem
+
++(CRSCompoundCoordinateReferenceSystem *) create{
+    return [[CRSCompoundCoordinateReferenceSystem alloc] init];
+}
+
+-(instancetype) init{
+    self = [super initWithType:CRS_TYPE_COMPOUND];
+    if(self != nil){
+        _coordinateReferenceSystems = [NSMutableArray array];
+    }
+    return self;
+}
+
+-(instancetype) initWithName: (NSString *) name andType: (enum CRSType) type{
+    self = [super initWithName:name andType:type];
+    if(self != nil){
+        _coordinateReferenceSystems = [NSMutableArray array];
+    }
+    return self;
+}
+
+-(int) numCoordinateReferenceSystems{
+    return (int) _coordinateReferenceSystems.count;
+}
+
+-(CRSSimpleCoordinateReferenceSystem *) coordinateReferenceSystemAtIndex: (int) index{
+    return [_coordinateReferenceSystems objectAtIndex:index];
+}
+
+-(void) setCoordinateReferenceSystems: (NSMutableArray<CRSSimpleCoordinateReferenceSystem *> *) coordinateReferenceSystems{
+    [_coordinateReferenceSystems removeAllObjects];
+    [self addCoordinateReferenceSystems:coordinateReferenceSystems];
+}
+
+-(void) addCoordinateReferenceSystem: (CRSSimpleCoordinateReferenceSystem *) crs{
+    switch(crs.type){
+        case CRS_TYPE_GEODETIC:
+        case CRS_TYPE_GEOGRAPHIC:
+        case CRS_TYPE_PROJECTED:
+        case CRS_TYPE_VERTICAL:
+        case CRS_TYPE_ENGINEERING:
+        case CRS_TYPE_PARAMETRIC:
+        case CRS_TYPE_TEMPORAL:
+        case CRS_TYPE_DERIVED:
+            [_coordinateReferenceSystems addObject:crs];
+            break;
+        default:
+            [NSException raise:@"Unsupported Compound CRS" format:@"Unsupported Compound Coordinate Reference System Type: %d", crs.type];
+    }
+}
+
+-(void) addCoordinateReferenceSystems: (NSArray<CRSSimpleCoordinateReferenceSystem *> *) crss{
+    for(CRSSimpleCoordinateReferenceSystem *crs in crss){
+        [self addCoordinateReferenceSystem:crs];
+    }
+}
+
+- (BOOL) equals: (CRSCompoundCoordinateReferenceSystem *) compoundCoordinateReferenceSystem{
+    if (self == compoundCoordinateReferenceSystem){
+        return YES;
+    }
+    if (compoundCoordinateReferenceSystem == nil){
+        return NO;
+    }
+    if (![super isEqual:compoundCoordinateReferenceSystem]){
+        return NO;
+    }
+    if (_coordinateReferenceSystems == nil) {
+        if (compoundCoordinateReferenceSystem.coordinateReferenceSystems != nil){
+            return NO;
+        }
+    } else if (![_coordinateReferenceSystems isEqual:compoundCoordinateReferenceSystem.coordinateReferenceSystems]){
+        return NO;
+    }
+    return YES;
+}
+
+- (BOOL) isEqual: (id) object{
+    if (self == object) {
+        return YES;
+    }
+    
+    if (![object isKindOfClass:[CRSCompoundCoordinateReferenceSystem class]]) {
+        return NO;
+    }
+    
+    return [self equals:(CRSCompoundCoordinateReferenceSystem *)object];
+}
+
+- (NSUInteger) hash{
+    NSUInteger prime = 31;
+    NSUInteger result = [super hash];
+    result = prime * result + ((_coordinateReferenceSystems == nil) ? 0 : [_coordinateReferenceSystems hash]);
+    return result;
+}
+
+@end
---
+++ crs-ios/CRSSimpleCoordinateReferenceSystem.m
@@ -0,0 +1,75 @@
+//
+//  CRSSimpleCoordinateReferenceSystem.m
+//  crs-ios
+//
+//  Created by Brian Osborn on 7/19/21.
+//  Copyright © 2021 NGA. All rights reserved.
+//
+
+#import "CRSSimpleCoordinateReferenceSystem.h"
+
+@implementation CRSSimpleCoordinateReferenceSystem
+
+-(instancetype) init{
+    self = [super init];
+    return self;
+}
+
+-(instancetype) initWithType: (enum CRSType) type{
+    self = [super initWithType:type];
+    return self;
+}
+
+-(instancetype) initWithName: (NSString *) name andType: (enum CRSType) type{
+    self = [super initWithName:name andType:type];
+    return self;
+}
+
+-(instancetype) initWithName: (NSString *) name andType: (enum CRSType) type andCoordinateSystem: (CRSCoordinateSystem *) coordinateSystem{
+    self = [super initWithName:name andType:type];
+    if(self != nil){
+        [self setCoordinateSystem:coordinateSystem];
+    }
+    return self;
+}
+
+- (BOOL) equals: (CRSSimpleCoordinateReferenceSystem *) simpleCoordinateReferenceSystem{
+    if (self == simpleCoordinateReferenceSystem){
+        return YES;
+    }
+    if (simpleCoordinateReferenceSystem == nil){
+        return NO;
+    }
+    if (![super isEqual:simpleCoordinateReferenceSystem]){
+        return NO;
+    }
+    if (_coordinateSystem == nil) {
+        if (simpleCoordinateReferenceSystem.coordinateSystem != nil){
+            return NO;
+        }
+    } else if (![_coordinateSystem isEqual:simpleCoordinateReferenceSystem.coordinateSystem]){
+        return NO;
+    }
+    return YES;
+}
+
+- (BOOL) isEqual: (id) object{
+    if (self == object) {
+        return YES;
+    }
+    
+    if (![object isKindOfClass:[CRSSimpleCoordinateReferenceSystem class]]) {
+        return NO;
+    }
+    
+    return [self equals:(CRSSimpleCoordinateReferenceSystem *)object];
+}
+
+- (NSUInteger) hash{
+    NSUInteger prime = 31;
+    NSUInteger result = [super hash];
+    result = prime * result + ((_coordinateSystem == nil) ? 0 : [_coordinateSystem hash]);
+    return result;
+}
+
+@end
