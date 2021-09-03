e0a143172a36dcd1295e19ce86adc95626af8243
Brian Osborn <bosborn@gmail.com>
2021-09-03 13:05:09 -0600

    common datums

--- crs-ios/geo/CRSGeoDatums.m
+++ crs-ios/geo/CRSGeoDatums.m
@@ -34,9 +34,17 @@ static NSMutableDictionary<NSString *, CRSGeoDatums *> *nameDatums = nil;
     typeDatums = [NSMutableDictionary dictionary];
     nameDatums = [NSMutableDictionary dictionary];
     
-    [self initializeDatum:[self createWithType:CRS_DATUM_WGS84 andCode:@"WGS84" andTranslationX:0 andTranslationY:0 andTranslationZ:0 andEllipsoid:nil andNames:[NSArray arrayWithObjects:@"WGS84", @"World Geodetic System 1984 ensemble", @"WGS 1984", @"WGS 84", nil]]];
-    // TODO
-    
+    [self initializeDatum:[self createWithType:CRS_DATUM_WGS84 andCode:@"WGS84" andTranslationX:0 andTranslationY:0 andTranslationZ:0 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_WGS84] andNames:[NSArray arrayWithObjects:@"WGS84", @"World Geodetic System 1984 ensemble", @"WGS 1984", @"WGS 84", nil]]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_GGRS87 andCode:@"GGRS87" andTranslationX:-199.87 andTranslationY:74.79 andTranslationZ:246.62 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_GRS80] andName:@"Greek Geodetic Reference System 1987"]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_NAD83 andCode:@"NAD83" andTranslationX:0 andTranslationY:0 andTranslationZ:0 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_GRS80] andName:@"North American Datum 1983"]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_NAD27 andCode:@"NAD27" andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_CLARKE_1866] andName:@"North American Datum 1927"]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_POTSDAM andCode:@"potsdam" andTranslationX:598.1 andTranslationY:73.7 andTranslationZ:418.2 andRotationX:0.202 andRotationY:0.045 andRotationZ:-2.455 andScaleDifference:6.7 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_BESSEL] andNames:[NSArray arrayWithObjects:@"Potsdam Rauenberg 1950 DHDN", @"Deutsches Hauptdreiecksnetz", nil]]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_CARTHAGE andCode:@"carthage" andTranslationX:-263.0 andTranslationY:6.0 andTranslationZ:431.0 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_CLARKE_1880] andNames:[NSArray arrayWithObjects:@"Carthage 1934 Tunisia", @"Carthage", nil]]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_HERMANNSKOGEL andCode:@"hermannskogel" andTranslationX:577.326 andTranslationY:90.129 andTranslationZ:463.919 andRotationX:5.137 andRotationY:1.474 andRotationZ:5.297 andScaleDifference:2.4232 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_BESSEL] andNames:[NSArray arrayWithObjects:@"Hermannskogel", @"Militar-Geographische Institut", @"Militar Geographische Institute", nil]]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_IRE65 andCode:@"ire65" andTranslationX:482.530 andTranslationY:-130.596 andTranslationZ:564.557 andRotationX:-1.042 andRotationY:-0.214 andRotationZ:-0.631 andScaleDifference:8.15 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_MOD_AIRY] andNames:[NSArray arrayWithObjects:@"Ireland 1965", @"TM65", nil]]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_NZGD49 andCode:@"nzgd49" andTranslationX:59.47 andTranslationY:-5.04 andTranslationZ:187.44 andRotationX:0.47 andRotationY:-0.1 andRotationZ:1.024 andScaleDifference:-4.5993 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_INTERNATIONAL] andName:@"New Zealand Geodetic Datum 1949"]];
+    [self initializeDatum:[self createWithType:CRS_DATUM_OSEB36 andCode:@"OSGB36" andTranslationX:446.448 andTranslationY:-125.157 andTranslationZ:542.060 andRotationX:0.1502 andRotationY:0.2470 andRotationZ:0.8421 andScaleDifference:-20.4894 andEllipsoid:[CRSEllipsoids fromType:CRS_ELLIPSOIDS_AIRY] andNames:[NSArray arrayWithObjects:@"Airy 1830", @"OSGB 1936", @"Ordnance Survey of Great Britain 1936", nil]]];
+
 }
 
 +(void) initializeDatum: (CRSGeoDatums *) datum{
